60a0d9ea6abac408845d16916cac9f791e736070
Wissam Moussa <wissam.moussa@cds-snc.ca>
2022-12-08 22:39:44 -0500

    Making my first signed commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-mod day7;
+mod day8;
 mod utils;
 use std::env;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
-    let data = utils::read_lines("./data/day7.txt");
-    println!("Part A: {}", day7::solve_a(&data));
-    println!("Part B: {}", day7::solve_b(&data));
+    let data = utils::read_lines("./test_data/day8.txt");
+    println!("Part A: {}", day8::solve_a(&data));
+    println!("Part B: {}", day8::solve_b(&data));
 }
